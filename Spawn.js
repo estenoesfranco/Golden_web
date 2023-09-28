8e964504a4fa3280fcf507533274e4cef71b05f1
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-09-27 23:35:12 -0300

    Add files via upload

--- Spawn.js
+++ Spawn.js
@@ -1,4 +1,18 @@
-function mostrarCarta() {
-    const carta = document.getElementById('carta');
-    carta.style.display = 'block';
-  }
+function mostrarCartaFranki() {
+  const cartaFranki = document.getElementById('cartaFranki');
+
+  cartaRonin.style.display = 'none';
+  // Muestra la carta de Franki
+  cartaFranki.style.display = 'block';
+}
+
+function mostrarOtraCartaRonin() {
+ 
+  const cartaRonin = document.getElementById('cartaRonin');
+
+  // Oculta la carta de Franki si está visible
+  cartaFranki.style.display = 'none';
+
+  // Muestra la carta de Ronin
+  cartaRonin.style.display = 'block';
+}
