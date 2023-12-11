84bcd8678ff74b6d7352856404e644a732cb275e
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-12-11 00:05:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Schema = mongoose.Schema;
+const port = 3000;
 
-const ejemploSchema = new Schema({
-  // Define tu esquema aquí
-});
-
-const EjemploModel = mongoose.model('Ejemplo', ejemploSchema);
-
-module.exports = EjemploModel;
 // Conexión a MongoDB
-mongoose.connect("tu_cadena_de_conexion", {
+mongoose.connect("mongodb+srv://frangamerdemon:queopinasdelarazaariaperonista4444@francluster.zudsgju.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+// Define tu esquema
+const ejemploSchema = new Schema({
+    nombre: String,
+    edad: Number,
+    // Agrega otros campos según sea necesario
+});
+
+// Crea el modelo usando el esquema
+const EjemploModel = mongoose.model('Ejemplo', ejemploSchema);
 
 // Verifica la conexión
 const db = mongoose.connection;
@@ -23,4 +27,22 @@ db.once("open", () => {
     console.log("Conexión exitosa a MongoDB");
 });
 
+app.use(express.json()); 
+app.post('/', async (req, res) => {
+  try {
+      console.log("Recibiendo solicitud POST:", req.body);
+      const nuevoEjemplo = new EjemploModel(req.body);
+      const resultado = await nuevoEjemplo.save();
+      console.log("Documento creado con éxito:", resultado);
+      res.status(201).json(resultado);
+  } catch (error) {
+      console.error("Error al crear el documento:", error);
+      res.status(500).json({ error: error.message });
+  }
+});
+
 // Resto de tu configuración de Express
+
+app.listen(port, () => {
+    console.log(`Me estoy ejecutando en el puerto ${port}`);
+});
