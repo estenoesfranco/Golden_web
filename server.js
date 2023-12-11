7969de7cc827d93a71787e286f29b89afeff727c
estenoesfranco <112403813+estenoesfranco@users.noreply.github.com>
2023-12-11 12:59:35 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,22 +3,13 @@ const mongoose = require("mongoose");
 const app = express();
 const Schema = mongoose.Schema;
 const port = 3000;
-
+const Schema4444 = require("./Controllers/information");
 // Conexión a MongoDB
 mongoose.connect("mongodb+srv://frangamerdemon:queopinasdelarazaariaperonista4444@francluster.zudsgju.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// Define tu esquema
-const ejemploSchema = new Schema({
-    nombre: String,
-    edad: Number,
-    // Agrega otros campos según sea necesario
-});
-
-// Crea el modelo usando el esquema
-const EjemploModel = mongoose.model('Ejemplo', ejemploSchema);
 
 // Verifica la conexión
 const db = mongoose.connection;
@@ -27,7 +18,14 @@ db.once("open", () => {
     console.log("Conexión exitosa a MongoDB");
 });
 
-app.use(express.json()); 
+app.use(express.json());
+
+app.get('/', async (req, res) =>{
+        console.log("DATO ENVIADO")
+        res.send(await Schema4444.find())
+
+})
+
 app.post('/', async (req, res) => {
   try {
       console.log("Recibiendo solicitud POST:", req.body);
